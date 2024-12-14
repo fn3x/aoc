0fa0d5fc1d02895a260f052ba6e44afba29ec889
Art P <fn3x@yandex.com>
2024-12-14 23:39:17 +0400

    feat: solved day 2

--- 2024/src/main.zig
+++ 2024/src/main.zig
@@ -7,6 +7,69 @@ pub fn main() !void {
 
     try day1(alloc, "src/1_1");
     try day2Of1("src/2");
+    try day2Of2(alloc, "src/2");
+}
+
+fn day2Of2(allocator: std.mem.Allocator, input_path: []const u8) !void {
+    const file = try std.fs.cwd().openFile(input_path, .{});
+    defer file.close();
+
+    var buf_reader = std.io.bufferedReader(file.reader());
+    const reader = buf_reader.reader();
+
+    var safe_reports: usize = 0;
+    var buf: [1024]u8 = undefined;
+
+    var container = std.ArrayList(usize).init(allocator);
+    defer container.deinit();
+
+    var is_report_safe: bool = undefined;
+
+    while (try reader.readUntilDelimiterOrEof(&buf, '\n')) |line| {
+        is_report_safe = false;
+        container.clearRetainingCapacity();
+
+        var reports = std.mem.splitSequence(u8, line, " ");
+        while (reports.next()) |report| {
+            const num = try std.fmt.parseInt(usize, report, 10);
+            try container.append(num);
+        }
+
+        var withoutSkipped = try allocator.alloc(usize, container.items.len - 1);
+        defer allocator.free(withoutSkipped);
+
+        inner: for (0..container.items.len) |skipIndex| {
+            std.mem.copyForwards(usize, withoutSkipped[0..skipIndex], container.items[0..skipIndex]);
+            std.mem.copyForwards(usize, withoutSkipped[skipIndex..], container.items[skipIndex + 1 ..]);
+
+            var previous_report: usize = withoutSkipped[0];
+            var current_report: usize = withoutSkipped[1];
+
+            const increasing = current_report > previous_report;
+
+            if (isReportDangerous(previous_report, current_report, increasing)) {
+                continue :inner;
+            }
+
+            for (2..withoutSkipped.len) |i| {
+                previous_report = current_report;
+                current_report = withoutSkipped[i];
+
+                if (isReportDangerous(previous_report, current_report, increasing)) {
+                    continue :inner;
+                }
+            }
+
+            is_report_safe = true;
+            break :inner;
+        }
+
+        if (is_report_safe) {
+            safe_reports += 1;
+        }
+    }
+
+    std.log.info("d2p2:: safe reports: {d}", .{safe_reports});
 }
 
 fn day2Of1(input_path: []const u8) !void {
@@ -42,23 +105,7 @@ fn day2Of1(input_path: []const u8) !void {
             previous_report = current_report;
             current_report = try std.fmt.parseInt(usize, num, 10);
 
-            if (current_report == previous_report) {
-                continue :outer;
-            }
-
-            if (current_report < previous_report and increasing) {
-                continue :outer;
-            }
-
-            if (current_report > previous_report and !increasing) {
-                continue :outer;
-            }
-
-            if (current_report > previous_report and current_report - previous_report > 3) {
-                continue :outer;
-            }
-
-            if (current_report < previous_report and previous_report - current_report > 3) {
+            if (isReportDangerous(previous_report, current_report, increasing)) {
                 continue :outer;
             }
         }
@@ -66,6 +113,30 @@ fn day2Of1(input_path: []const u8) !void {
     }
 
     std.log.info("d2p1:: safe reports: {d}", .{safe_reports});
+}
+
+fn isReportDangerous(previous_report: usize, current_report: usize, increasing: bool) bool {
+    if (current_report == previous_report) {
+        return true;
+    }
+
+    if (current_report < previous_report and increasing) {
+        return true;
+    }
+
+    if (current_report > previous_report and !increasing) {
+        return true;
+    }
+
+    if (current_report > previous_report and current_report - previous_report > 3) {
+        return true;
+    }
+
+    if (current_report < previous_report and previous_report - current_report > 3) {
+        return true;
+    }
+
+    return false;
 }
 
 fn day1(alloc: std.mem.Allocator, input_path: []const u8) !void {
